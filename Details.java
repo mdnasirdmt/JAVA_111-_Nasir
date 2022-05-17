7e79494ed8b56cf23ce5a766dcfc62d61dbf0294
Md Nasir Uddin <mdnasirdmt@gmail.com>
2022-05-17 20:41:53 +0530

    my_details day1

---
+++ Details.java
@@ -0,0 +1,51 @@
+
+
+//1.    JDK (Java Development Kit) is a software development kit to develop applications in Java. In addition to JRE, JDK also contains number of development tools (compilers, JavaDoc, //Java Debugger etc.)
+
+
+//2.    JRE (Java Runtime Environment) is the implementation of JVM and is defined as a software package that provides Java class libraries, along with Java Virtual Machine (JVM), and other //components to run applications written in Java programming.
+
+
+
+//3.    JVM (Java Virtual Machine) is an abstract machine that is platform-dependent and has three notions as a specification, a document that describes requirement of JVM implementation, //implementation, a computer program that meets JVM requirements, and instance, an implementation that executes Java byte.
+
+
+
+
+//1.    J2SE is the Java 2 Standard Edition that contains your basic core Java classes. This is the one that most people use to write your standard applets and applications.
+
+
+//2.    J2EE is the Java 2 Enterprise Edition and it contains classes that go above and beyond J2SE. In fact, you will need J2SE in order to use many of the classes in J2EE. Some of the things //that J2EE provides are server-side classes such as Servlets and EJB's. It also contains Security API, Java Mail API, XML Parsers etc., Java Messaging Service API, and a few others. A few of //these API's have been included in the new J2SE 1.4 version and are now considered standard. The XML API is an example of this.
+
+
+
+//3.     J2ME is for developers that code to portable devices, such as a palm pilot or a cellular phone. Code on these devices needs to be small in size and take less memory. So you will want //to use J2ME if you code for a PDA.
+
+
+
+
+
+//Java is platform-independent because it uses a virtual machine. The Java programming language and all APIs are compiled into bytecodes. Bytecodes are effectively platform-independent. //The virtual machine takes care of the differences between the bytecodes for the different platforms.
+
+
+//What are the major features of Java programming?
+//Object Oriented. In Java, everything is an Object. ...
+//Platform Independent. ...
+//Simple. ...
+//Secure. ...
+//Architecture-neutral. ...
+//Portable. ...
+//Robust. ...
+//Multithreaded.
+
+
+
+public class Details{
+
+	public static void main (String[]args){
+		System.out.println("Name :	Md Nasir Uddin\n"+"Father's Name :	Md Inamul Haque\n"+"Mother 's Name :	Sony Khatoon\n"+"Age :	15.04.1995\n"+"Gender :	male\n"+"Address :	Bihar\n"+"Mobile :	8709639497\n");
+	}
+
+}
+
+
